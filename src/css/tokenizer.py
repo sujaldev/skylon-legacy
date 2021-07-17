b6fd37bc1bdb20ae95d08a23ee33840df404fac6
Sujal Singh <email.sujalsingh@gmail.com>
2021-07-17 08:22:42 +0530

    completed generate_new_token function

--- src/css/tokenizer.py
+++ src/css/tokenizer.py
@@ -46,6 +46,38 @@ class CSSTokenizer:
             "token-type": token_name
         }
 
+        # TOKENS THAT HAVE A VALUE
+        tokens_with_value_param = [
+            "indent-token",
+            "function-token",
+            "at-keyword-token",
+            "hash-token",
+            "string-token",
+            "url-token",
+
+            # DELIM TOKEN'S VALUE CAN ONLY CONTAIN ONE CHAR
+            "delim-token"
+        ]
+
+        tokens_with_numeric_value_param = [
+            "number-token",
+            "percentage-token",
+            "dimension-token"
+        ]
+
+        if token_name in tokens_with_value_param:
+            self.token_buffer["value"] = ""
+        elif token_name in tokens_with_numeric_value_param:
+            self.token_buffer["numeric-value"] = None
+
+        if token_name == "hash-token":
+            self.token_buffer["type-flag"] = "unrestricted"  # DEFAULT VALUE FOR HASH TOKEN'S TYPE FLAG OTHER IS ID
+        elif token_name == "number-token":
+            self.token_buffer["type-flag"] = "integer"  # DEFAULT IS INTEGER AND OTHER IS NUMBER
+        elif token_name == "dimension-token":
+            self.token_buffer["type-flag"] = "integer"  # DEFAULT IS INTEGER AND OTHER IS NUMBER
+            self.token_buffer["unit"] = ""  # CONTAINS ONE OR MORE CHARACTERS
+
     def consume(self, step=1):
         # UPDATE CHARACTERS
         if self.index < len(self.stream):
